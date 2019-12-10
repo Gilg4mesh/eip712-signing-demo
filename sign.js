2cbe5d83d724d1fb99889623543507daceb81eed
Gilg4mesh <freedomtomdestiny@gmail.com>
2019-12-10 16:43:28 +0800

    Sample for DelegateTransferToken

--- sign.js
+++ sign.js
@@ -42,42 +42,41 @@ window.onload = function (e) {
       { name: "version", type: "string" },
       { name: "chainId", type: "uint256" },
       { name: "verifyingContract", type: "address" },
-      { name: "salt", type: "bytes32" },
     ];
 
     const bid = [
-      { name: "amount", type: "uint256" },
-      { name: "bidder", type: "Identity" },
-    ];
-
-    const identity = [
-      { name: "userId", type: "uint256" },
-      { name: "wallet", type: "address" },
+      { name: 'delegate', type: 'address' },
+      { name: 'token', type: 'address' },
+      { name: 'nonce', type: 'uint256' },
+      { name: 'from', type: 'address' },
+      { name: 'to', type: 'address' },
+      { name: 'value', type: 'uint256' },
+      { name: 'fee', type: 'uint256' },
     ];
 
     const chainId = parseInt(web3.version.network, 10);
   
     const domainData = {
-      name: "My amazing dApp",
-      version: "2",
+      name: "DelegateTransferToken",
+      version: "1",
       chainId: chainId,
-      verifyingContract: "0x1C56346CD2A2Bf3202F771f50d3D14a367B48070",
-      salt: "0xf2d857f4a3edcb9b78b4d503bfe733db1e3f6cdc2b7971ee739626c97e86a558"
+      verifyingContract: "0x1C56346CD2A2Bf3202F771f50d3D14a367B48070"
     };
 
     var message = {
-      amount: 100,
-      bidder: {
-        userId: 323,
-        wallet: "0x3333333333333333333333333333333333333333"
-      }
+      delegate: "0x3333333333333333333333333333333333333333",
+      token: "0x3333333333333333333333333333333333333333",
+      nonce: 1,
+      from: "0x3333333333333333333333333333333333333333",
+      to: "0x3333333333333333333333333333333333333333",
+      value: 0,
+      fee: 0
     };
     
     const data = JSON.stringify({
       types: {
         EIP712Domain: domain,
         Bid: bid,
-        Identity: identity,
       },
       domain: domainData,
       primaryType: "Bid",
--- solidityCode.js
+++ solidityCode.js
@@ -6,40 +6,28 @@ pragma solidity ^0.5.0;
 contract Verifier {
     uint256 constant chainId = <CHAINID>;
     address constant verifyingContract = 0x1C56346CD2A2Bf3202F771f50d3D14a367B48070;
-    bytes32 constant salt = 0xf2d857f4a3edcb9b78b4d503bfe733db1e3f6cdc2b7971ee739626c97e86a558;
     
-    string private constant EIP712_DOMAIN  = "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract,bytes32 salt)";
-    string private constant IDENTITY_TYPE = "Identity(uint256 userId,address wallet)";
-    string private constant BID_TYPE = "Bid(uint256 amount,Identity bidder)Identity(uint256 userId,address wallet)";
+    string private constant EIP712_DOMAIN  = "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)";
+    string private constant BID_TYPE = "Bid(address delegate,address token,uint256 nonce,address from,address to,uint256 value,uint256 fee)";
     
     bytes32 private constant EIP712_DOMAIN_TYPEHASH = keccak256(abi.encodePacked(EIP712_DOMAIN));
-    bytes32 private constant IDENTITY_TYPEHASH = keccak256(abi.encodePacked(IDENTITY_TYPE));
     bytes32 private constant BID_TYPEHASH = keccak256(abi.encodePacked(BID_TYPE));
     bytes32 private constant DOMAIN_SEPARATOR = keccak256(abi.encode(
         EIP712_DOMAIN_TYPEHASH,
-        keccak256("My amazing dApp"),
-        keccak256("2"),
+        keccak256("DelegateTransferToken"),
+        keccak256("1"),
         chainId,
-        verifyingContract,
-        salt
+        verifyingContract
     ));
     
-    struct Identity {
-        uint256 userId;
-        address wallet;
-    }
-    
     struct Bid {
-        uint256 amount;
-        Identity bidder;
-    }
-    
-    function hashIdentity(Identity memory identity) private pure returns (bytes32) {
-        return keccak256(abi.encode(
-            IDENTITY_TYPEHASH,
-            identity.userId,
-            identity.wallet
-        ));
+        address delegate;
+        address token;
+        uint256 nonce;
+        address from;
+        address to;
+        uint256 value;
+        uint256 fee;
     }
     
     function hashBid(Bid memory bid) private pure returns (bytes32){
@@ -48,21 +36,26 @@ contract Verifier {
             DOMAIN_SEPARATOR,
             keccak256(abi.encode(
                 BID_TYPEHASH,
-                bid.amount,
-                hashIdentity(bid.bidder)
+                bid.delegate,
+                bid.token,
+                bid.nonce,
+                bid.from,
+                bid.to,
+                bid.value,
+                bid.fee
             ))
         ));
     }
     
     function verify() public pure returns (bool) {
-        Identity memory bidder = Identity({
-            userId: 323,
-            wallet: 0x3333333333333333333333333333333333333333
-        });
-        
         Bid memory bid = Bid({
-            amount: 100,
-            bidder: bidder
+            delegate: 0x3333333333333333333333333333333333333333,
+            token: 0x3333333333333333333333333333333333333333,
+            nonce: 1,
+            from: 0x3333333333333333333333333333333333333333,
+            to: 0x3333333333333333333333333333333333333333,
+            value: 0,
+            fee: 0
         });
             
         bytes32 sigR = <SIGR>;
